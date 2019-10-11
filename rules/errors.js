84d0be329addb83bb70ad68199f0b241f95c4817
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-10 21:29:17 -0400

    Add error rules

---
+++ rules/errors.js
@@ -0,0 +1,72 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'for-direction': 'error',
+    'getter-return': [
+      'error',
+      {
+        allowImplicit: true,
+      },
+    ],
+    'no-async-promise-executor': 'error',
+    'no-await-in-loop': 'error',
+    'no-compare-neg-zero': 'error',
+    'no-cond-assign': ['error', 'always'],
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'no-constant-condition': 'error',
+    'no-control-regex': 'error',
+    'no-debugger': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-keys': 'error',
+    'no-duplicate-case': 'error',
+    'no-empty': 'error',
+    'no-empty-character-class': 'error',
+    'no-ex-assign': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-func-assign': 'error',
+    'no-import-assign': 'error',
+    'no-inner-declarations': ['error', 'both'],
+    'no-invalid-regexp', [
+      'error',
+      {
+        allowConstructorFlags: ['u', 'y']
+      }
+    ],
+    'no-irregular-whitespace': [
+      'error',
+      {
+        skipStrings: true,
+        skipTemplates: true,
+      },
+    ],
+    'no-misleading-character-class': 'error',
+    'no-obj-calls': 'error',
+    'no-prototype-builtins': 'error',
+    'no-regex-spaces': 'error',
+    'no-sparse-arrays': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'no-unsafe-finally': 'error',
+    'no-unsafe-negation': 'error',
+    'require-atomic-updates': 'error',
+    'use-isnan': [
+      'error',
+      {
+        enforceForSwitchCase: true,
+      },
+    ],
+    'valid-typeof': [
+      'error',
+      {
+        requireStringLiterals: true,
+      },
+    ],
+  },
+};
