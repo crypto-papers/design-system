757606d2d7cd3a661c935d68bf5e64d05462b870
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-11 00:02:08 -0400

    Add ES6 rules

---
+++ rules/es6.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = {
+  env: {
+    es6: true,
+  },
+  rules: {
+    'constructor-super': 'error',
+    'no-class-assign': 'error',
+    'no-const-assign': 'error',
+    'no-dupe-class-members': 'error',
+    'no-duplicate-imports': [
+      'error',
+      {
+        includeExports: true,
+      },
+    ],
+    'no-new-symbol': 'error',
+    'no-restricted-imports': 'off',
+    'no-this-before-super': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-constructor': 'error',
+    'no-useless-rename': [
+      'error',
+      {
+        ignoreDestructuring: false,
+        ignoreExport: false,
+        ignoreImport: false,
+      },
+    ],
+    'no-var': 'warn',
+    'object-shorthand': [
+      'error',
+      'always',
+      {
+        avoidExplicitReturnArrows: true,
+        avoidQuotes: true,
+        ignoreConstructors: true,
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+    'prefer-destructuring': [
+      'error',
+      {
+        AssignmentExpression: {
+          array: true,
+          object: false,
+        },
+        enforceForRenamedProperties: false,
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+      },
+    ],
+    'prefer-numeric-literals': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'require-yield': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['all', 'single', 'multiple', 'none'],
+      },
+    ],
+    'symbol-description': 'error',
+  },
+};
