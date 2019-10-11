57af0081c2dd3f721e5af4e9d9d9196ee901d756
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-10 20:09:40 -0400

    Add Node.js rules

---
+++ rules/nodeCommon.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  rules: {
+    'callback-return': 'warn',
+    'global-require': 'error',
+    'handle-callback-err': ['error', '^.*(e|E)rr'],
+    'no-buffer-constructor': 'error',
+    'no-mixed-requires': 'error',
+    'no-new-require': 'error',
+    'no-path-concat': 'error',
+    'no-process-env': 'error',
+    'no-process-exit': 'error',
+    'no-restricted-modules': 'off',
+    'no-sync': 'off',
+  },
+};
