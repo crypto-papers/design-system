9d070600b22c7345ab05e44a8e69de114e3dbfaf
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-13 14:31:18 -0400

    Use short version of plugin name

--- react.js
+++ react.js
@@ -4,11 +4,9 @@ const reactVersion = '16.10.2';
 
 module.exports = {
   extends: [
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
     'eslint-config-prettier/react',
   ],
-  plugins: ['eslint-plugin-a11y', 'eslint-plugin-react', 'eslint-plugin-react-hooks'],
+  plugins: ['jsx-a11y', 'react', 'react-hooks'],
   rules: {},
   settings: {
     react: {
