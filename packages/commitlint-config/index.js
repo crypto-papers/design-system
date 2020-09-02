ad6fbe16f0fa202f46afdc586693c6216432f91d
Marek Rewers <mrewers@users.noreply.github.com>
2020-09-01 20:02:31 -0400

    Fix(commitlint): Spread rule values into rule object
    Rather than using require resolve to load each ruleset individually, combine them into one object.

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -1,12 +1,20 @@
-const rules = [
-  './rules/body',
-  './rules/footer',
-  './rules/header',
-  './rules/misc',
-  './rules/scope',
-  './rules/subject',
-  './rules/type',
-].map(require.resolve);
+const body = require('./rules/body');
+const footer = require('./rules/footer');
+const header = require('./rules/header');
+const misc = require('./rules/misc');
+const scope = require('./rules/scope');
+const subject = require('./rules/subject');
+const type = require('./rules/type');
+
+const rules = {
+  ...body.rules,
+  ...footer.rules,
+  ...header.rules,
+  ...misc.rules,
+  ...scope.rules,
+  ...subject.rules,
+  ...type.rules,
+};
 
 module.exports = {
   rules,
