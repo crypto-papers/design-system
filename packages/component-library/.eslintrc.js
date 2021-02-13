084c7f370ea67fa616fa74010da692a7a961a494
Marek Rewers <hello@marek.dev>
2021-02-13 01:48:53 -0500

    Build(component): Add ESLint config

---
+++ packages/component-library/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    "@cryptopapers/eslint-config",
+    "@cryptopapers/eslint-config/react",
+    "@cryptopapers/eslint-config/typescript",
+  ],
+  ignorePatterns: ["lib/"],
+};
