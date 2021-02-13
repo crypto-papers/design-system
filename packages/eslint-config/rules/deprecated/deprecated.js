f75a039b3d59ed15ce3c66595fafbe2051aa34a6
Marek Rewers <hello@marek.dev>
2021-02-13 16:33:59 -0500

    Feat(eslint): Add missing indent-legacy to deprecated rules

--- packages/eslint-config/rules/deprecated/deprecated.js
+++ packages/eslint-config/rules/deprecated/deprecated.js
@@ -31,6 +31,7 @@ module.exports = {
     'lines-around-directive': 'off', // ESLint - v4.0.0 - padding-line-between-statements
     'newline-after-var': 'off', // ESLint - v4.0.0 - padding-line-between-statements
     'newline-before-return': 'off', // ESLint - v4.0.0 - padding-line-between-statements
+    'indent-legacy': 'off', // ESLint - v4.0.0 - indent
     'prefer-reflect': 'off', // ESLint - v3.9.0
     'no-native-reassign': 'off', // ESLint - v3.3.0 - no-global-assign
     'no-negated-in-lhs': 'off', // ESLint - v3.3.0 - no-unsafe-negation
