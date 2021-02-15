d32a115567987cbd03c73d2bd3dc5fd34b38568c
Marek Rewers <hello@marek.dev>
2021-02-15 18:49:38 -0500

    Build: Move TypeScript linting to the project root

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  extends: ['@cryptopapers/eslint-config'],
-  ignorePatterns: ['node_modules'],
+  extends: ['@cryptopapers/eslint-config', '@cryptopapers/eslint-config/react'],
+  ignorePatterns: ['node_modules/', 'packages/component-library/lib/*'],
+  overrides: [
+    {
+      extends: [
+        '@cryptopapers/eslint-config',
+        '@cryptopapers/eslint-config/react',
+        '@cryptopapers/eslint-config/typescript',
+      ],
+      files: ['**/*.ts', '**/*.tsx'],
+    },
+  ],
 };
--- packages/component-library/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    "@cryptopapers/eslint-config",
-    "@cryptopapers/eslint-config/react",
-    "@cryptopapers/eslint-config/typescript",
-  ],
-  ignorePatterns: ["lib/"],
-};
